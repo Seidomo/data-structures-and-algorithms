e222f8e8be8becfc5e4c4c55b21dd180c91b50ba
seido <seid783@gmail.com>
2021-03-08 07:03:22 -0800

    code challenge

--- challenges/tree/tree.js
+++ challenges/tree/tree.js
@@ -7,7 +7,7 @@
 // class KaryNode {
 //   constructor(value, size) {
 //     this.value = value;
-//     this.children = new Array(size); 
+//     this.children = new Array(size);
 //   }
 // }
 
@@ -56,77 +56,152 @@ class Tree {
     this.root = null;
   }
 
-  traverse() {
 
-    // recusive helper function
+  preOrder() {
+
+    let current = this.root;
+    const arr = [];
     const preOrder = (node) => {
+      arr[arr.length] = node.value;
+      if (node.left) { preOrder(node.left); }
+      if (node.right) { preOrder(node.right); }
+    };
 
-      console.log(node.value);
+    preOrder(current);
+    return arr;
 
-      if (node.left) {
-        preOrder(node.left);
-      }
 
-      if (node.right) {
-        preOrder(node.right);
+  }
+
+  postOrder() {
+
+    let current = this.root;
+    const arr = [];
+    const postOrder = (node) => {
+      if (node.left) { postOrder(node.left); }
+      if (node.right) { postOrder(node.right); }
+      arr[arr.length] = node.value;
+    };
+
+    postOrder(current);
+    return arr;
+
+
+  }
+
+
+  inOrder() {
+
+    let current = this.root;
+    const arr = [];
+    const inOrder = (node) => {
+      if (node.left) { inOrder(node.left); }
+      arr[arr.length] = node.value;
+      if (node.right) { inOrder(node.right); }
+    };
+
+    inOrder(current);
+    return arr;
+
+
+  }
+
+
+  maxValue() {
+    let current = this.root;
+    if (!current) { throw 'error'; }
+
+    const findMax = (node) => {
+      if (!node) { return; }
+      let value = node.value;
+      let leftValue = findMax(node.left);
+      let rightValue = findMax(node.right);
+      if (leftValue > value) { value = leftValue; }
+      if (rightValue > value) { value = rightValue; }
+      return value;
+    };
+    return findMax(current);
+  }
+
+
+}
+
+
+
+class BinaryTree{
+  constructor(){
+    this.root = null;
+  }
+
+
+  add(value) {
+    let current = this.root;
+    let newNode = new Node(value);
+
+    if (!current) { return this.root = newNode; }
+
+    const insertNode = (node, newNode) => {
+      if (newNode.value < node.value) {
+        if (!node.left) { node.left = newNode; }
+        else { insertNode(node.left, newNode); }
+      } else {
+        if (!node.right) { node.right = newNode; }
+        else { insertNode(node.right, newNode); }
       }
     };
 
+    insertNode(current, newNode);
+  }
+
+  inOrder() {
     let current = this.root;
-    preOrder(current);
+    const arr = [];
+    const inOrder = (node) => {
+      if (node.left) { inOrder(node.left); }
+      arr[arr.length] = node.value;
+      if (node.right) { inOrder(node.right); }
+    };
+
+    inOrder(current);
+    return arr;
   }
 
-  traverseWithStack() {
+  contains(value) {
     let current = this.root;
-    let stack = [];
-    stack.push(current);
-
-    while (stack.length) {
-
-      console.log(current.value);
-
-
-      if (current.right) {
-        stack.push(current.right);
-      }
-      if (current.left) {
-        stack.push(current.left);
-      }
-
-      current = stack.pop();
-    }
+    const map = {};
+    const inOrder = (node) => {
+      if (node.left) { inOrder(node.left); }
+      map[node.value] = node.value;
+      if (node.right) { inOrder(node.right); }
+    };
+    inOrder(current);
+    return !!(map[value]);
   }
 
-  breadth() {
 
-    let current = null;
-    let queue = [];
-
-    queue.unshift(this.root);
-
-    while (queue.length) {
-
-      current = queue.pop();
-      console.log(current.value);
-
-      if (current.left) {
-        queue.unshift(current.left);
-      }
-      if (current.right) {
-        queue.unshift(current.right);
-      }
-    }
-  }
 }
 
-const tree = new Tree();
+// const trees = new BinaryTree();
+// const tree = new Tree();
 
-tree.root = new Node(5);
-tree.root.left = new Node(10);
-tree.root.left.left = new Node(0);
-tree.root.left.right = new Node(20);
-tree.root.right = new Node(15);
-tree.root.right.right = new Node(25);
+// tree.root = new Node(1);
+// tree.root.left = new Node(2);
+// tree.root.left.left = new Node(3);
+// tree.root.left.right = new Node(4);
+// tree.root.right = new Node(5);
+// tree.root.right.right = new Node(6);
+
+// trees.add(1);
+// trees.add(2);
+// trees.add(3);
+// trees.add(4);
+
+// console.log(tree.preOrder());
+// console.log(tree.postOrder());
+// console.log(tree.inOrder());
+// console.log(trees.inOrder());
+// console.log(trees.contains(3));
+
 
 // tree.traverse();
 // tree.traverseWithStack();
@@ -141,3 +216,8 @@ tree.root.right.right = new Node(25);
 // karyTree.root.children[0].children[0] = new KaryNode(5, karyTree.k);
 
 // karyTree.breadth();
+module.exports = {
+  Node,
+  Tree,
+  BinaryTree
+};
