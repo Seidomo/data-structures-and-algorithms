56b78aa27076d548d0e2c6a19beaeecb9e4773b2
seido <seid783@gmail.com>
2021-03-29 22:07:04 -0700

    Code Challenge

--- challenges/__tests__/tree-intersection.test.js
+++ challenges/__tests__/tree-intersection.test.js
@@ -4,33 +4,33 @@
 
 const trees = require('../treeIntersection/tree-intersection.js');
 
-describe('testing tree intersection function', () =>{
+describe('testing finding common value in the tree ', () =>{
 
-  let root1 = trees.Tree();
-  root1.root = trees.Node(50);
-  root1.root.left = trees.Node(100);
-  root1.root.left.left = trees.Node(150);
-  root1.root.left.right = trees.Node(200);
-  root1.root.left.right.left = trees.Node(250);
-  root1.root.left.right.right = trees.Node(300);
-  root1.root.right = trees.Node(350);
-  root1.root.right.left = trees.Node(400);
-  root1.root.right.right = trees.Node(450);
-  root1.root.right.right.left = trees.Node(500);
-  root1.root.right.right.right = trees.Node(550);
+  let root1 = new trees.Tree();
+  root1.root = new trees.Node(50);
+  root1.root.left = new trees.Node(100);
+  root1.root.left.left = new trees.Node(150);
+  root1.root.left.right = new trees.Node(200);
+  root1.root.left.right.left = new trees.Node(250);
+  root1.root.left.right.right = new trees.Node(300);
+  root1.root.right = new trees.Node(350);
+  root1.root.right.left = new trees.Node(400);
+  root1.root.right.right = new trees.Node(450);
+  root1.root.right.right.left = new trees.Node(500);
+  root1.root.right.right.right = new trees.Node(550);
 
-  let root2 = trees.Tree();
-  root2.root = trees.Node(50);
-  root2.root.left = trees.Node(100);
-  root2.root.left.left = trees.Node(120);
-  root2.root.left.right = trees.Node(180);
-  root2.root.left.right.left = trees.Node(200);
-  root2.root.left.right.right = trees.Node(300);
-  root2.root.right = trees.Node(330);
-  root2.root.right.left = trees.Node(400);
-  root2.root.right.right = trees.Node(410);
-  root2.root.right.right.left = trees.Node(490);
-  root2.root.right.right.right = trees.Node(500);
+  let root2 = new trees.Tree();
+  root2.root = new trees.Node(50);
+  root2.root.left = new trees.Node(100);
+  root2.root.left.left = new trees.Node(120);
+  root2.root.left.right = new trees.Node(180);
+  root2.root.left.right.left = new trees.Node(200);
+  root2.root.left.right.right = new trees.Node(300);
+  root2.root.right = new trees.Node(330);
+  root2.root.right.left = new trees.Node(400);
+  root2.root.right.right = new trees.Node(410);
+  root2.root.right.right.left = new trees.Node(490);
+  root2.root.right.right.right = new trees.Node(500);
 
 
   test( 'should get an array of common values', () =>{
